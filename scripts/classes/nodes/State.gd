f02247cb695b021ce8e072cc4cea6b1ecfefb78c
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-09-26 19:15:45 -0500

    Can pause StateMachine
    Changed StateMachine to use _get_property_list

--- scripts/classes/nodes/State.gd
+++ scripts/classes/nodes/State.gd
@@ -1,64 +1,58 @@
-## A self-contained state, to be used with StateMachine
+## A self-contained state, to be used with @class StateMachine
 extends Node
 class_name State
 
-## Indicates that the state should be changed
-# @arg{int}  new_state  An integer denoting the desired state
+## Indicates that the state should be changed.
+# @arg  int  new_state  An integer denoting the desired state
+# @desc                 The @class StateMachine reacts to this signal and
+#                       changes to the state indiciated by @i new_state.
 signal state_change_request(new_state)
 
-## A custom signal type
-# @arg{string}  signal_name  A string with the name of a custom signal
-# @arg{array}   args         A list of parameters to pass
-signal state_custom_signal(signal_name, args)
+## A signal that gets communicated to the @class StateMachine parent and causes it to do something.
+# @arg  String  name  A string used to identify the call
+# @arg  Array   args  A list of arguments to pass to the function
+# @desc               The @class StateMachine parent will connect to this signal automatically.
+#                     What happens next depends on the @a name parameter. (See the documentation
+#                     for @class StateMachine for details).
+signal state_parent_call(name, args)
 
-## Tells the StateMachine parent to enable part of the active state
-# @arg{int} mode  A constant of enum @e StateMachine::StateCallMode
-signal state_enable(mode)
-
-## Tells the StateMachine parent to disable part of the active state
-# @arg{int} mode  A constant of enum @e StateMachine::StateCallMode
-signal state_disable(mode)
-
-## Queue a state for activation
-# @arg{int}  state  The next state to add to queue
-signal queue_add_state(state)
-
-## The persistant state, from which this state is acted upon
 var persistant_state: Object
-
-## User data, passed to each state
 var user_data
 
 # warning-ignore-all:shadowed_variable
 
-## Called when exiting a state
+## Called just before exiting the state.
 # @virtual
-# @note     Override this function to implement a custom destructor
 func cleanup() -> void: pass
 
-## Returns a metadata value with a default as fallback
-# @arg    name     The key for which a metadata is defined
-# @arg    default  The default return value if @a name does not exist
-# @return          The metadata for @a name (if it exists) or @a default otherwise
+## Gets a meta value or a default on failure.
+# @const
+# @desc   If the meta field @a name exists, that is returned; otherwise
+#         @a default is returned.
 func get_meta_or_default(name: String, default = null):
 	if has_meta(name):
 		return get_meta(name)
 	return default
 
-## The main fixed-step physics process for the state
+## Should be called during the physics step.
 # @virtual
-# @arg      delta  The delta step in seconds
+# @desc    Override this method to implement custom physics-step behavior.
 func physics_main(_delta: float): pass
 
-## The main idle-frame process for the state
+## Should be called during the idle step.
 # @virtual
-# @arg      delta  The delta step in seconds
+# @desc    Override this method to implement custom idle-step behavior.
 func process_main(_delta: float): pass
 
-## Called when entering a state, used for initialization
-# @virtual
-# @arg     persistant_state  The object modified by the state
-# @arg     user_data         User-provided data, if any
+## Called on the first frame of a state.
+# @desc  Call this function to initialize the state.
+#        @a persistant_state is the persistent state, that is, the object which
+#        this state modifies. @a user_data is forwarded to the state and can be
+#        used by it.
+#
+#        @b{Note:} if the method @function _setup is defined, it is called
+#        right at the end of this function. So it can be used to implement
+#        state-specific initialization.
 func setup(persistant_state, user_data = null) -> void:
 	self.persistant_state = persistant_state
 	self.user_data = user_data
--- scripts/classes/nodes/StateMachine.gd
+++ scripts/classes/nodes/StateMachine.gd
@@ -1,136 +1,179 @@
 ## Container and controller of states
-# @desc  A StateMachine controls the state of an object.
-#        Any child that is a State will be added to an internal array of states.
-#        An integer index is used to control which state is active through
-#        the @c change_state function.
-extends Node
+# @desc  A StateMachine controls the state of an object, referred internally as the persistent state.
+#        The state machine collects all its @class State children and compiles them
+#        into an array. From there, they are accessible by functions like @function change_state.
+#
+#        It is necessary that you call @function change_state at least once before calling
+#        @function state_physics or @function state_process.
+#
+#        While the @class StateMachine collects a list of its @class State children, it also
+#        connects to a number of signals as well. The signals it connects to are as follows.
+#
+#        * state_change_request(int new_state) @br
+#        Change to a different state. @a new_state is the index of the state to switch to.
+#
+#        * state_parent_call(String name, Array args) @br
+#        Invoke an action in the @class StateMachine parent. @br
+#        If @a name is "disable" or "enable", disable or enable a portion of the
+#        active state, respectively. Index 0 of @a args is a constant of @enum StateCallMode.
+tool
 class_name StateMachine
+extends Node
 
-signal state_custom_signal(signal_name, signal_args)
+## Path to the root node
+# @type  NodePath
+# @desc  The root node of the @class StateMachine. Referred by the @class State
+#        children as the persistent state.
+var root_node: NodePath
 
-## The node being manipulated by the states
-export(NodePath) var root_node: NodePath
+## Whether the @class StateMachine is paused or not
+# @type   bool
+# @setter set_paused
+var paused := false setget set_paused
 
-## State call mode
-# @constant  PHYSICS  Physics process
-# @constant  PROCESS  Idle process
+## Which part of a state to enable or disable
+# @constant PHYSICS 0 Physics process
+# @constant PROCESS 1 Idle process
 enum StateCallMode {PHYSICS, PROCESS}
 
 var user_data setget set_user_data
 var do_physics: FuncRef
 var do_process: FuncRef
-var state_queue := []
 
 var _states: = []
 var _current_state: int = -1
 
+func _dummy_state(_delta): pass
+
 func _exit_tree() -> void:
+	if Engine.editor_hint: return
+	
 	for s in _states:
 		(s as Node).queue_free()
 
+func _get(property: String):
+	match property:
+		'root_node':
+			return root_node
+		'paused':
+			return paused
+
+func _get_property_list() -> Array:
+	return [
+		{
+			name = 'StateMachine',
+			type = TYPE_NIL,
+			usage = PROPERTY_USAGE_CATEGORY | PROPERTY_USAGE_SCRIPT_VARIABLE
+		},
+		{
+			name = 'root_node',
+			type = TYPE_NODE_PATH,
+			usage = PROPERTY_USAGE_DEFAULT
+		},
+		{
+			name = 'paused',
+			type = TYPE_BOOL,
+			usage = PROPERTY_USAGE_DEFAULT
+		}
+	]
+
 func _ready():
+	if Engine.editor_hint: return
+	
 	for state in get_children():
-		if not (state is State):
-			push_warning("node \"%s\" is not a State" % state.name)
-			continue
+		assert(state is State)
 		var s: State = state
-		s.connect("state_change_request", self, "_change_state", [], CONNECT_DEFERRED)
-		s.connect("queue_add_state", self, "queue_add_state")
-		s.connect('state_custom_signal', self, '_process_custom_signal')
-		s.connect("state_disable", self, "_state_enable_disable", [false])
-		s.connect('state_enable', self, '_state_enable_disable', [true])
+		s.connect('state_change_request', self, "_change_state", [], CONNECT_DEFERRED)
+		s.connect('state_parent_call', self, '_state_parent_call')
 		_states.append(s)
 		remove_child(s)
 
-## Switch to a different state
-# @arg{int}  next_state  The numerical index of the state to switch to
-# @note                  For a given state, the parameter will be the child index for the state.
-#                        So if the state is the first child of StateMachine, this would be 0.
-# @return                A constant of the enumeration @enum Error
+func _set(property: String, value) -> bool:
+	match property:
+		'root_node':
+			root_node = value
+		'paused':
+			paused = value
+		_:
+			return false
+	return true
+
+## Change to a different state
+# @desc  Call this function to switch to a different state in the machine. @a next_state
+#        is the index of the state you wish to go to. The states are in the same order
+#        as they appear in the editor, starting from zero.
+#
+#        Before switching to a new state, the old state's @function cleanup method is invoked.
+#        Then after switching to the new state, its @function setup method is invoked.
 func change_state(next_state: int) -> int:
 	var old_state = _current_state
 	if next_state < 0 or next_state >= _states.size():
 		push_error(str("invalid index ", next_state))
 		return ERR_PARAMETER_RANGE_ERROR
-	
+
 	# Call the cleanup function of the old state
 	if old_state >= 0:
 		(_states[old_state] as State).cleanup()
-	
+
 	# Call the setup function of the new state
 	_current_state = next_state
-	
+
 	# Reference the physics and idle functions before initializing state
 	var temp: State = _states[_current_state]
 	do_physics = funcref(temp, "physics_main")
 	do_process = funcref(temp, "process_main")
 	temp.setup(get_node(root_node), user_data)
-	
+
 	return OK
 
-## Get the current (active) state
-# @return  The current state as @type int
+## Get the current state
 func current_state() -> int: return _current_state
 
-## Sets the user data that gets passed to each state
+func set_paused(value: bool) -> void:
+	paused = value
+
+# Set the user data forwarded to the state
+# @desc  @a udata can be any type. The user data is forwarded to the
+#        state when it is initialized.
 func set_user_data(udata) -> void: user_data = udata
 
-## Call a specific method with optional parameters to the active state
-# @arg{string}  method  The name of the method to call
-# @arg{array}   args    A list of parameters to forward to the method
-# @return               The result of @a method
+# Call a state-specific method
+# @desc  Calls @a method in the current state, provided it exists. @a args
+#        are forwarded to @a method.
 func state_call(method: String, args: Array = []):
+	if paused: return
 	var result
 	var state: State = _states[_current_state]
 	if state.has_method(method):
 		result = state.callv(method, args)
 	return result
 
-## Adds the given state to the back of the queue
-# @arg{int}  state  Integer index of a state
-# @note             If state is out of range, an error is emitted
-# @return           A constant of @enum Error
-func queue_add_state(state: int) -> int:
-	if state < 0 or state >= _states.size():
-		push_error(str("Invalid state: ", state))
-		return ERR_PARAMETER_RANGE_ERROR
-	
-	state_queue.push_front(state)
-	
-	return OK
+## Go to the state's physics method
+func state_physics(delta: float):
+	if paused: return
+	assert(do_physics.is_valid())
+	return do_physics.call_func(delta)
 
-## Returns true if the queue is empty
-# @return  True or false depending on whether the queue is empty
-func queue_is_empty() -> bool: return state_queue.empty()
-
-## Switches to the next state in the queue, if any
-# @return  The result of @function change_state
-func queue_next_state() -> int:
-	if queue_is_empty():
-		push_error('No states loaded in the queue')
-		return FAILED
-	
-	return change_state(state_queue.pop_back())
+## Go to the state's process method
+func state_process(delta: float):
+	if paused: return
+	assert(do_process.is_valid())
+	return do_process.call_func(delta)
 
 # Signal callbacks
 
 func _change_state(new_state: int):
 	change_state(new_state)
 
-func _dummy_state(_delta): pass
-
-func _process_custom_signal(signal_name: String, signal_args: Array) -> void:
-	emit_signal('state_custom_signal', signal_name, signal_args)
-
-func _state_enable_disable(enable: bool, mode: int) -> void:
-	match mode:
-		StateCallMode.PHYSICS:
-			do_physics = funcref(self, 'physics_main' if enable else '_dummy_state')
-		StateCallMode.PROCESS:
-			do_process = funcref(self, 'process_main' if enable else '_dummy_state')
-		_:
-			var fields := []
-			fields.append(mode)
-			fields.append_array([StateCallMode.PHYSICS, "StateCallMode.PHYSICS"])
-			fields.append_array([StateCallMode.PROCESS, "StateCallMode.PROCESS"])
-			push_error("invalid mode %d, must be %d (%s) or %d (%s)" % fields)
+func _state_parent_call(n: String, args: Array) -> void:
+	match n:
+		"disable":
+			if args[0] == StateCallMode.PHYSICS:
+				do_physics = funcref(self, '_dummy_state')
+			elif args[0] == StateCallMode.PROCESS:
+				do_process = funcref(self, '_dummy_state')
+		"enable":
+			if args[0] == StateCallMode.PHYSICS:
+				do_physics = funcref(self, 'physics_main')
+			elif args[0] == StateCallMode.PROCESS:
+				do_process = funcref(self, 'process_main')
