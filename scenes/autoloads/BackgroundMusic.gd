363fb91fd6039e639aa4a1e5d5e32d950d707c3b
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-10-15 22:07:39 -0500

    Moved autoload scenes to their own folder

--- scenes/autoloads/BackgroundMusic.gd
+++ scenes/autoloads/BackgroundMusic.gd
@@ -1,7 +1,17 @@
+## Singleton for playing background music.
+# @name BackgroundMusic
+# @singleton
 extends AudioStreamPlayer
 
-export var songs: Dictionary
+## A list of songs each mapped to a name.
+# @type Dictionary
+# @desc  This holds a bunch of songs, mapping each name of a file relative
+#        to @constant MUSIC_PATH. Each call to @function play_music() is expects
+#        a key from this dictionary.
+export(Dictionary) var songs
 
+## Path to the music folder
+# @type String
 const MUSIC_PATH := "res://assets/audio/music"
 
 onready var tween = $Tween
@@ -9,7 +19,10 @@ onready var tween = $Tween
 func _enter_tree() -> void:
 	stream = null
 
-func fade_out(duration: float):
+## Fades the music out.
+# @desc  Call this function to fade the music over a period of time specified
+#        by @a duration seconds. Once the fade is complete, the music stops.
+func fade_out(duration: float) -> void:
 	if not is_playing() or tween.is_active(): return
 	if duration <= 0.0:
 		push_error("invalid duration %f, needs to be greater than 0" % duration)
@@ -17,6 +30,10 @@ func fade_out(duration: float):
 	tween.interpolate_property(self, @"volume_db", null, -80.0, duration, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	tween.start()
 
+## Play a song from the mapping.
+# @desc  Plays a song from the mapping specified by @a song. If @a position is specified
+#        and is greater than zero, marks the position in the song (in seconds) to start
+#        playback.
 func play_music(song: String, position: float = 0.0) -> void:
 	var audio_stream = songs.get(song)
 	
--- scenes/autoloads/TransitionRect.gd
+++ scenes/autoloads/TransitionRect.gd
@@ -1,21 +1,23 @@
 ## Singleton used to control fade in and fade out effects.
+# @name      TransitionRect
+# @singleton
+# @desc      This class displays a black rectangle on top of the scene at all times. At the start,
+#            it is invisible so as not to cover the screen. Functions can be called to fade the
+#            screen in and out by interpolating the alpha of the black rectnggle.
+#
+#            The functions @function fade_in() and @function fade_out() are used to implement
+#            traditional fade in and fade out effects.
 extends CanvasLayer
 
-## @name TransitionRect
-
-## Signifies that the fade is finished
+## Emitted when a fade has finished.
+# @arg String type  Describes which kind of fade was finished, "in" or "out"
 signal fade_finished(type)
 
-## The default duration of a fade
+## The default duration of a fade.
+# @type float
 const DEFAULT_DURATION := 1.0
 
 onready var tween = $Tween
-
-# element: [String scene, bool deferred]
-var _queued_scene := []
-
-# element: [String method, Dictionary options]
-var _queued_fade := []
 
 func _ready() -> void:
 	var screen_size := Vector2(
@@ -43,7 +45,7 @@ func _ready() -> void:
 #        the user. For it to be valid, the initial value must be in the range [0.01, 1.0].
 func fade_in(options: Dictionary = {}) -> void:
 	if tween.is_active(): return
-	
+
 	# duration of the fade
 	var duration := DEFAULT_DURATION
 	if 'duration' in options:
@@ -51,22 +53,22 @@ func fade_in(options: Dictionary = {}) -> void:
 		var temp: float = options['duration']
 		if temp > 0.0:
 			duration = temp
-	
+
 	# initial value
 	var initial_value := 1.0
 	if 'initial_value' in options:
 		var temp: float = options['initial_value']
 		if Math.is_in_range(temp, 0.01, 1.0):
 			initial_value = temp
-	
+
 	print("interpolate alpha between %f and %f in %f seconds"
 		% [initial_value, 0.0, duration])
-	
+
 	# start tween
 	tween.interpolate_property($ColorRect, @"self_modulate:a",
-		initial_value, 0.0, duration, Tween.TRANS_LINEAR)
+		initial_value, 0.0, duration, Tween.TRANS_CUBIC)
 	tween.start()
-	
+
 	set_meta('current_fade', 'in') # fade type
 
 ## Start a fade out effect.
@@ -86,7 +88,7 @@ func fade_in(options: Dictionary = {}) -> void:
 #        the user. For it to be valid, the initial value must be in the range [0.0, 0.99].
 func fade_out(options: Dictionary = {}) -> void:
 	if tween.is_active(): return
-	
+
 	# duration of the fade
 	var duration := DEFAULT_DURATION
 	if 'duration' in options:
@@ -94,59 +96,33 @@ func fade_out(options: Dictionary = {}) -> void:
 		var temp: float = options['duration']
 		if temp > 0.0:
 			duration = temp
-	
+
 	# initial value
 	var initial_value: float = $ColorRect.self_modulate.a
 	if initial_value >= 1.0:
 		initial_value = 0.0
-	
+
 	# option overrides initial_value
 	if 'initial_value' in options:
 		var temp: float = options['initial_value']
 		if Math.is_in_range(temp, 0.00, 0.99):
 			initial_value = temp
-	
+
 	print("interpolate alpha between %f and %f in %f seconds"
 		% [initial_value, 1.0, duration])
-	
+
 	# start tween
 	tween.interpolate_property($ColorRect, @"self_modulate:a",
-		initial_value, 1.0, duration, Tween.TRANS_LINEAR)
+		initial_value, 1.0, duration, Tween.TRANS_CUBIC)
 	tween.start()
-	
+
 	set_meta('current_fade', 'out') # fade type
-
-func queue_fade(type: String, options: Dictionary = {}) -> void:
-	assert(type in ['in', 'out'], "invalid fade type, must be 'in' or 'out'")
-	_queued_fade.push_back(['fade_' + type, options])
-
-func queue_next_scene(scene: String, deferred: bool = false) -> void:
-	_queued_scene.push_back([scene, deferred])
 
 func _on_all_tweens_completed() -> void:
 	var cf: String = get_meta('current_fade')
 	emit_signal('fade_finished', cf)
-	if cf == 'out':
-		_load_queued_scene()
 
 func _go_to_next_scene(scene: String):
 	var t := get_tree()
 	t.connect('idle_frame', self, '_start_queued_fade', [], CONNECT_ONESHOT)
 	t.change_scene(scene)
-
-func _load_queued_scene() -> void:
-	var ar = _queued_scene.pop_front()
-	if ar:
-		var scene: String = ar[0]
-		var deferred: bool = ar[1]
-		if deferred:
-			call_deferred('_go_to_next_scene', scene)
-		else:
-			_go_to_next_scene(scene)
-
-func _start_queued_fade() -> void:
-	var ar = _queued_fade.pop_front()
-	if ar:
-		var m: String = ar[0]
-		var o: Dictionary = ar[1]
-		call(m, o)
