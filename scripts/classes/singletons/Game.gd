cf5ba911f3466e7442bc762440d73b778d7b8a49
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-10-25 23:40:41 -0500

    Update documentation
    Added documentation to Game singleton

--- scripts/classes/singletons/Game.gd
+++ scripts/classes/singletons/Game.gd
@@ -1,18 +1,37 @@
+## Game-wide functions and values.
+# @name Game
+# @singleton
 extends Node
 
-# TODO: add documentation
-
+## Indicates that a game parameter has changed.
+# @arg String  param The parameter that was changed.
+# @arg Variant value The value @a param was changed to.
+# @desc This signal is emitted when certain functions are called. Which function
+#       was called is indicated by @a param.
+#
+#       Param: dialog_mode@br
+#       Type:  bool@br
+#       Emitted when @function set_dialog_mode() is called. If true, then
+#       dialog mode is active: in this mode, a dialog is in process, so actors
+#       should not move or accept input.
+#
+#       Param: tree_paused@br
+#       Type:  bool@br
+#       Emitted when @function set_paused() is called.
 signal changed_game_param(param, value)
 
+## Collision layer bits
 enum CollisionLayer {
-	WALLS,
-	PLATFORMS,
-	PLAYER,
-	ENEMY,
-	PLAYER_HITBOX,
-	PLAYER_HURTBOX,
-	ENEMY_HITBOX,
-	ENEMY_HURTBOX
+	WALLS = 0x01, ## Walls
+	PLATFORMS = 0x02, ## Platforms
+	PLAYER = 0x04, ## Player foreground collision
+	ENEMY = 0x08, ## Enemy foreground collision
+	PLAYER_HITBOX = 0x10, ## Player hitbox
+	PLAYER_HURTBOX = 0x20, ## Player hurtbox
+	ENEMY_HITBOX = 0x40, ## Enemy hitbox
+	ENEMY_HURTBOX = 0x80, ## Enemy hurtbox,
+	NPC = 0x0100, ## NPC foreground collision
+	NPC_DETECTION = 0x0200 ## NPC detection field
 }
 
 enum ActorIDS {
@@ -26,7 +45,7 @@ enum ActorIDS {
 var default_kevin_speed := Vector2(85, 275)
 var level_size := Vector2(1020, 610)
 var dialog_mode := false setget set_dialog_mode
-var global_scale := Vector2.ONE setget set_global_scale
+#var global_scale := Vector2.ONE setget set_global_scale
 
 const Scenes := {
 	pause_screen = preload("res://scenes/gui/PauseScreen.tscn"),
@@ -71,20 +90,30 @@ func is_key_pressed(e: InputEventKey, key: int, echo: bool = false):
 			if not e.echo: return true
 	return false
 
-func set_global_scale(scale := Vector2(1, 1)):
-	var vp := get_tree().root
-	var ct := vp.canvas_transform
-	
-	# Create transformation matrix
-	var t := Transform2D.IDENTITY.scaled(scale)
-	t.origin = ct.origin
-	
-	vp.set_canvas_transform(t)
+#func set_global_scale(scale := Vector2(1, 1)):
+#	var vp := get_tree().root
+#	var ct := vp.canvas_transform
+#
+#	# Create transformation matrix
+#	var t := Transform2D.IDENTITY.scaled(scale)
+#	t.origin = ct.origin
+#
+#	vp.set_canvas_transform(t)
 
+## Set or clear dialog mode.
+# @desc Depending on whether @a enabled is true or false, this sets or disables
+#       dialog mode. In dialog mode, affected nodes stop working and do not
+#       accept input from the user, allowing dialog boxes to work without interruption.
+#
+#       Emits the @code changed_game_param signal.
 func set_dialog_mode(enabled: bool):
 	dialog_mode = enabled
 	emit_signal("changed_game_param", "dialog_mode", enabled)
 
+## Set the pause status of the scene tree.
+# @desc Pauses the scene tree if @a paused is true or unpauses it otherwise.
+#
+#       Emits the @code changed_game_param signal.
 func set_paused(paused: bool) -> void:
 	get_tree().paused = paused
 	emit_signal("changed_game_param", "tree_paused", paused)
