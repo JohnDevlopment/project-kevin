0882ff52430ec10c0879940b92b5541290474608
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-10-26 00:05:31 -0500

    Added npc and npc detection layers; modified dialog test scene

--- scenes/DialogTest.gd
+++ scenes/DialogTest.gd
@@ -7,10 +7,7 @@ func _enter_tree() -> void:
 
 func _ready() -> void:
 	TransitionRect.fade_in({duration = 1.0})
-	yield( get_tree().create_timer(1), 'timeout' )
-	$Villager1.start_dialog()
-	#var dlg = Dialogic.start('villager1_start')
-	#add_child(dlg)
+	$CanvasLayer/DebugOverlay.add_stat('Kevin Detected', $NPCBase, '_kevin_detected', false)
 
 func _on_Kevin_sprint_meter_update_parameters(min_value, max_value) -> void:
 	sprint_meter.min_value = min_value
