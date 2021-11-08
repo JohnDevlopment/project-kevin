58a1f6b4cf5f898a2c20aeb6525598b7b6fd7d0f
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-08 08:46:03 -0600

    Removed temp files

--- assets/tilesets/Grassland_BG.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Grassland BG" tilewidth="16" tileheight="16" tilecount="24" columns="4">
- <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
- <image source="../textures/tilesets/grassland/grassland_background.png" width="64" height="96"/>
-</tileset>
