bee72f6b93875f710c3ab395449b258902495c67
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-12 22:56:15 -0600

    Inheritence-based level design, and new level called "Village"

--- scenes/levels/LevelBase.gd
+++ scenes/levels/LevelBase.gd
@@ -5,8 +5,19 @@ export var level_limits := Vector2() setget set_level_limits
 
 var draw_level_limits := false
 
-#func _set(property: String, value) -> bool:
-#	pass
+func _get(property: String):
+	match property:
+		'editor/draw_level_limits':
+			return draw_level_limits
+
+func _set(property: String, value) -> bool:
+	match property:
+		'editor/draw_level_limits':
+			set_draw_level_limits(value)
+		_:
+			return false
+	
+	return true
 
 func _get_property_list() -> Array:
 	return [
@@ -19,12 +30,38 @@ func _get_property_list() -> Array:
 
 func _draw() -> void:
 	if Engine.editor_hint:
-		if draw_level_limits:
-			var drawing_rect := Rect2(Vector2(), level_limits)
-			draw_rect(drawing_rect, Color.black)
+		if draw_level_limits and level_limits != Vector2.ZERO:
+			var drawing_rect := Rect2(Vector2(), level_limits * Game.TILE_SIZE)
+			draw_rect(drawing_rect, Color.black, false)
 
+func _ready() -> void:
+	if not Engine.editor_hint:
+		Game.connect('changed_game_param', self, '_on_game_param_changed')
+		
+		Game.set_level_size(level_limits)
+		
+		# If player exists, 
+		if Game.has_player():
+			var kevin := Game.get_player()
+			kevin.disable_input = true
+			yield(get_tree().create_timer(1), 'timeout')
+			kevin.disable_input = false
+		
+		# Fade into scene
+		Game.set_paused(true)
+		TransitionRect.fade_in({duration = 2.0})
+		yield(TransitionRect, 'fade_finished')
+		Game.set_paused(false)
 
+func set_draw_level_limits(v: bool) -> void:
+	draw_level_limits = v
+	update()
 
 func set_level_limits(v: Vector2) -> void:
-	level_limits = v
+	level_limits = v.round()
 	update()
+
+# Signals
+
+func _on_game_param_changed(_param: String, _value):
+	pass
